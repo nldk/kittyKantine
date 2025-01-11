9f5edd20b352cbfde9069b2bad8a47714f6ceb8d
Niel <niel.dekeyser@gmail.com>
2025-01-11 11:37:07 +0100

    v1.1

--- app/src/main/java/com/example/mqtt/MainActivity.kt
+++ app/src/main/java/com/example/mqtt/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mqtt
 
+import android.app.Activity
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,11 +18,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,46 +38,95 @@ import java.nio.charset.StandardCharsets
 
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         try {
+            var firstStart = true
+            try {
+                load(applicationContext, "firstStart")
+                firstStart = false
+            } catch (e: Exception) {
 
-        }
-        super.onCreate(savedInstanceState)
-        val host = ""
-        val username = ""
-        val password = ""
-
-        val client: Mqtt5Client = Mqtt5Client.builder().identifier(generateRandomString(20))
-            .serverHost(host).automaticReconnectWithDefaultConfig()
-            .serverPort(8883)
-            .sslWithDefaultConfig()
-            .build()
-
-        client.toBlocking().connectWith().simpleAuth()
-            .username(username)
-            .password(password.toByteArray(StandardCharsets.UTF_8)).applySimpleAuth()
-            .willPublish()
-            .topic("home/will").payload("sensor gone".toByteArray()).applyWillPublish().send()
-
-        runBlocking {
-            launch {
-                client.toAsync().subscribeWith().topicFilter("kattenvoederbak/#").callback { publish ->
-                    if(publish.payloadAsBytes.toString(StandardCharsets.UTF_8)=="sluit"){
-                        messageState = "gesloten"
-                    }else if (publish.payloadAsBytes.toString(StandardCharsets.UTF_8)=="open"){
-                        messageState = "open"
-                    }
-                }.send()
             }
-        }
+            super.onCreate(savedInstanceState)
+            val host = ""
+            var username = ""
+            var password = ""
+            var messageState by mutableStateOf("not updated")
+            lateinit var client: Mqtt5Client
 
-        enableEdgeToEdge()
-        setContent {
-            MqttTheme {
+            if (!firstStart) {
+                username = load(applicationContext,"username")
+                password = load(applicationContext,"pass")
+                client = Mqtt5Client.builder().identifier(load(applicationContext,"id"))
+                    .serverHost(host).automaticReconnectWithDefaultConfig()
+                    .serverPort(8883)
+                    .sslWithDefaultConfig()
+                    .build()
+
+                client.toBlocking().connectWith().simpleAuth()
+                    .username(username)
+                    .password(password.toByteArray(StandardCharsets.UTF_8)).applySimpleAuth()
+                    .willPublish()
+                    .topic("home/will").payload("sensor gone".toByteArray()).applyWillPublish()
+                    .send()
+
+                runBlocking {
+                    launch {
+                        client.toAsync().subscribeWith().topicFilter("kattenvoederbak/#")
+                            .callback { publish ->
+                                if (publish.payloadAsBytes.toString(StandardCharsets.UTF_8) == "sluit") {
+                                    messageState = "gesloten"
+                                } else if (publish.payloadAsBytes.toString(StandardCharsets.UTF_8) == "open") {
+                                    messageState = "open"
+                                }
+                            }.send()
+                    }
+                }
+            }
+            enableEdgeToEdge()
+            setContent {
+                MqttTheme {
+                    if (!firstStart) {
+                        Column {
+                            Spacer(modifier = Modifier.height(35.dp))
+                            Hok(messageState, client)
+                        }
+                    } else {
+                        var name by remember { mutableStateOf("") }
+                        var Password by remember { mutableStateOf("") }
+                        Column {
+                            Spacer(modifier = Modifier.height(35.dp))
+                            Text(text = "username")
+                            OutlinedTextField(value = name, onValueChange = { text ->
+                                name = text
+                            })
+                            Text(text = "password")
+                            OutlinedTextField(value = Password, onValueChange = { text ->
+                                Password = text
+                            })
+                            Button(
+                                onClick = {
+                                    username = name
+                                    password = Password
+                                    save(applicationContext, "firstStart", "false")
+                                    save(applicationContext,"username",name)
+                                    save(applicationContext,"pass",password)
+                                    save(applicationContext,"id",generateRandomString(20))
+                                    recreate()
+                                },
+                            ) {
+                                Text("OK")
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            enableEdgeToEdge()
+            setContent {
                 Column {
                     Spacer(modifier = Modifier.height(35.dp))
-                    Hok(messageState,client)
+                    Text(e.toString())
                 }
             }
         }
@@ -81,7 +134,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Hok(state:String,client: Mqtt5Client) {
+fun Hok(state: String, client: Mqtt5Client) {
     Box(Modifier.height(50.dp)) {
         Row {
             Spacer(modifier = Modifier.width(10.dp))
@@ -89,29 +142,40 @@ fun Hok(state:String,client: Mqtt5Client) {
                 onClick = {
                     runBlocking {
                         launch {
-                            client.toBlocking().publishWith().topic("kattenvoederbak/klep").payload("sluit".toByteArray()).send()
+                            client.toBlocking().publishWith().topic("kattenvoederbak/klep")
+                                .payload("sluit".toByteArray()).send()
                         }
                     }
                 },
-                modifier = Modifier.fillMaxHeight().weight(5.0F)
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(5.0F)
             ) {
                 Text("sluiten")
             }
             Spacer(modifier = Modifier.width(10.dp))
             Column {
                 Spacer(modifier = Modifier.height(12.dp))
-                Text(state, modifier = Modifier.fillMaxHeight().width(65.dp).align(Alignment.CenterHorizontally))
+                Text(
+                    state, modifier = Modifier
+                        .fillMaxHeight()
+                        .width(65.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
             }
             Spacer(modifier = Modifier.width(10.dp))
             Button(
                 onClick = {
                     runBlocking {
                         launch {
-                            client.toBlocking().publishWith().topic("kattenvoederbak/klep").payload("open".toByteArray()).send()
+                            client.toBlocking().publishWith().topic("kattenvoederbak/klep")
+                                .payload("open".toByteArray()).send()
                         }
                     }
                 },
-                modifier = Modifier.fillMaxHeight().weight(5.0F)
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(5.0F)
             ) {
                 Text("openen")
             }
@@ -119,22 +183,34 @@ fun Hok(state:String,client: Mqtt5Client) {
         }
     }
 }
-fun generateRandomString(len: Int = 15): String{
+
+fun generateRandomString(len: Int = 15): String {
     val alphanumerics = CharArray(26) { it -> (it + 97).toChar() }.toSet()
         .union(CharArray(9) { it -> (it + 48).toChar() }.toSet())
-    return (0..len-1).map {
+    return (0..len - 1).map {
         alphanumerics.toList().random()
     }.joinToString("")
 }
-fun save(context: Context, filename:String, content:String){
+
+fun save(context: Context, filename: String, content: String) {
     val fileContents = content
     context.openFileOutput(filename, MODE_PRIVATE).use {
         it.write(fileContents.toByteArray())
     }
 }
-fun load(context: Context, filename:String):String{
+
+fun load(context: Context, filename: String): String {
     val content = context.openFileInput(filename).bufferedReader().use {
         it.readText()
     }
     return content
+}
+fun triggerRestart(context: Activity) {
+    val intent = Intent(context, MainActivity::class.java)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    context.startActivity(intent)
+    if (context is Activity) {
+        (context as Activity).finish()
+    }
+    Runtime.getRuntime().exit(0)
 }
