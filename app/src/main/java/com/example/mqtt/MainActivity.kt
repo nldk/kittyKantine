b501e0d7998dd9cf3a79744979d2b35840bfd3e0
Niel <niel.dekeyser@gmail.com>
2025-01-21 20:50:24 +0100

    V1.1

--- app/src/main/java/com/example/mqtt/MainActivity.kt
+++ app/src/main/java/com/example/mqtt/MainActivity.kt
@@ -8,27 +8,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.mqtt.ui.theme.MqttTheme
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client
@@ -89,7 +89,12 @@ class MainActivity : ComponentActivity() {
                     if (!firstStart) {
                         Column {
                             Spacer(modifier = Modifier.height(35.dp))
-                            Hok(messageState, client)
+                            Row {
+                                Icon(Icons.Default.Settings,contentDescription = null,Modifier.clickable {
+
+                                })
+                                Hok(messageState, client)
+                            }
                         }
                     } else {
                         var name by remember { mutableStateOf("") }
@@ -106,11 +111,9 @@ class MainActivity : ComponentActivity() {
                             })
                             Button(
                                 onClick = {
-                                    username = name
-                                    password = Password
                                     save(applicationContext, "firstStart", "false")
                                     save(applicationContext,"username",name)
-                                    save(applicationContext,"pass",password)
+                                    save(applicationContext,"pass",Password)
                                     save(applicationContext,"id",generateRandomString(20))
                                     recreate()
                                 },
@@ -204,13 +207,4 @@ fun load(context: Context, filename: String): String {
         it.readText()
     }
     return content
-}
-fun triggerRestart(context: Activity) {
-    val intent = Intent(context, MainActivity::class.java)
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    context.startActivity(intent)
-    if (context is Activity) {
-        (context as Activity).finish()
-    }
-    Runtime.getRuntime().exit(0)
 }
